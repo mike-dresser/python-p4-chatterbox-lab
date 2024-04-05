2398048ba017e944968bbadbfc8e06c2f33a32c8
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-04 20:39:22 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -14,13 +14,39 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        all_messages = Message.query.order_by('created_at').all()
+        result = []
+        for message in all_messages:
+            result.append(message.to_dict())
+        return result, 200
+    elif request.method == 'POST':
+        json_data = request.get_json()
+        new_msg = Message(
+            body = json_data.get('body'),
+            username = json_data.get('username')
+        )
+        db.session.add(new_msg)
+        db.session.commit()
+        return new_msg.to_dict(), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    msg = Message.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return msg.to_dict(), 200
+    elif request.method == 'PATCH':
+        json_data = request.get_json()
+        setattr(msg, 'body', json_data['body'])
+        db.session.add(msg)
+        db.session.commit()
+        return msg.to_dict(), 200
+    elif request.method == 'DELETE':
+        db.session.delete(msg)
+        db.session.commit()
+        return {}, 204
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -12,3 +12,13 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+# class User(db.Model, SerializerMixin):
+#     __tablename__ = 'users'
+
+#     id = db.Column(db.String, primary_key=True)
+#     username = db.username
